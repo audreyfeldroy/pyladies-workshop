1fab6c450508f617dbf48c2636cd8451ed2906fa
Audrey M Roy <audreyr@gmail.com>
2011-05-14 03:07:49 -0700

    Added debugging sample file.

---
+++ breakout-sessions/debugging/syntax_errors.py
@@ -0,0 +1,46 @@
+print "Watch out for syntax errors"
+
+to = "Sandy"
+from = 'Audrey'
+
+for i in range(5):
+    print i
+
+for j in range(10)
+    print j
+
+for k in range(5):
+    print k
+        print k
+
+print "Hi"
+print ""
+print "
+
+'''This is a multi-line string that
+spans more than 1 line because it 
+is long
+'''
+
+"""You can use double or single
+quotes; it doesn't matter
+"""
+
+''' But don't forget to close these
+multi-line strings properly
+''
+'
+
+sandy_list = [1, 2, 3]
+audrey_list = [1, 2, 3,]
+jess_list = ["1", "2", 3]
+esther_list = [1 2 3]
+
+n = 5
+if n == 6:
+    print "I don't know why, but n is now 6"
+elif n = 5:
+    print "Yes, n is still 5"
+else:
+    print "Something fishy is going on"
+
